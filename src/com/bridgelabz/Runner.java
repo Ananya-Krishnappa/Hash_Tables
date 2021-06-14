1b3ff62aac552340b21f9a7718e2def096d352a7
Ananya-Krishnappa <ananyak3395@gmail.com>
2021-06-14 22:24:08 +0530

    Added:find frequency of words in paragraph

--- src/com/bridgelabz/Runner.java
+++ src/com/bridgelabz/Runner.java
@@ -3,7 +3,7 @@ package com.bridgelabz;
 public class Runner {
 	public static void main(String[] args) {
 		MyHashTable<String, Integer> myHashTable = new MyHashTable<>();
-		String str = "To be or not to be";
+		String str = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
 		String[] strArray = str.split(" ");
 		for (int i = 0; i < strArray.length; i++) {
 			Integer value = myHashTable.get(strArray[i]);
